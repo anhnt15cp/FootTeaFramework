40621a8a9cc2bf5fb23aaeb206eecd094ba7ccc0
Tuan Anh <>
2025-04-01 13:13:16 +0700

    First commit

---
+++ FoodTea/FoodTea/FoodTea.h
@@ -0,0 +1,18 @@
+//
+//  FoodTea.h
+//  FoodTea
+//
+//  Created by ntq on 1/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FoodTea.
+FOUNDATION_EXPORT double FoodTeaVersionNumber;
+
+//! Project version string for FoodTea.
+FOUNDATION_EXPORT const unsigned char FoodTeaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoodTea/PublicHeader.h>
+
+
